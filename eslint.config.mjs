7c9cdc82ed2f9ac06dbb37cdf119ef62704452e7
mahmud444t <ferdausemahmud@gmail.com>
2024-11-28 12:11:38 +0600

    fixing deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,30 +2,20 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config} */
-export default {
-  files: ['**/*.{js,mjs,cjs,ts}'], // Apply to all JS/TS files
-
-  languageOptions: {
-    parserOptions: {
-      ecmaVersion: 2020, // ECMAScript version
-      sourceType: 'module', // Allow import/export
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['node_modules', 'dist'],
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
     },
-    globals: globals.node, // Node.js globals
   },
-
-  extends: [
-    pluginJs.configs.recommended, // JS recommended rules
-    ...tseslint.configs.recommended.map((config) => config), // TS recommended rules
-  ],
-
-  ignorePatterns: ['node_modules', 'dist'], // Files to ignore
-
-  rules: {
-    'no-unused-vars': 'error',
-    'no-unused-expressions': 'error',
-    'prefer-const': 'error',
-    'no-console': 'warn',
-    'no-undef': 'error',
-  },
-};
+];
